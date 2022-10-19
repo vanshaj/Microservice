253497ef72dd2e90f611462bedbb8c40c80ccbc0
vanshaj <vanshajdhar@gmail.com>
2022-10-19 12:07:57 +0530

    Completed Exercises

--- cliApps/todoapp/cmd/todo/main.go
+++ cliApps/todoapp/cmd/todo/main.go
@@ -17,32 +17,47 @@ func main() {
 	task := flag.String("task", "", "Task to be included in the todo list")
 	complete := flag.String("complete", "", "Item to be completed")
 	add := flag.Bool("add", false, "Add Task to the Todo List")
+	del := flag.Bool("del", false, "Delete the task from the list")
+	listAll := flag.Bool("listall", false, "List only incomplete items")
 	flag.Parse()
 
 	l := &todo.List{}
+	l.Get(fileName)
 
 	switch {
 	case *add:
-		l.Get(fileName)
 		taskData, err := getTask(os.Stdin, flag.Args()...)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		l.Add(taskData)
+		for _, eachTask := range taskData {
+			l.Add(eachTask)
+		}
+		if err := l.Save(fileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *del:
+		taskData, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		for _, eachTask := range taskData {
+			l.Delete(eachTask)
+		}
 		if err := l.Save(fileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 	case *list:
-		l.Get(fileName)
 		for _, item := range *l {
 			if item.Done != true {
 				fmt.Println(item.Task)
 			}
 		}
 	case *complete != "":
-		l.Get(fileName)
 		if err := l.Complete(*complete); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -54,27 +69,31 @@ func main() {
 			fmt.Printf("%s task has been completed", *complete)
 		}
 	case *task != "":
-		l.Get(fileName)
 		l.Add(*task)
 		if err := l.Save(fileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *listAll:
+		for _, val := range *l {
+			fmt.Println(val.Task)
+		}
 	}
 
 }
 
-func getTask(r io.Reader, args ...string) (string, error) {
+func getTask(r io.Reader, args ...string) ([]string, error) {
 	if len(args) > 0 {
-		return strings.Join(args, " "), nil
+		return []string{strings.Join(args, " ")}, nil
 	}
 	scanner := bufio.NewScanner(r)
-	scanner.Scan()
-	if err := scanner.Err(); err != nil {
-		return "", err
+	output := make([]string, 0, 20)
+	for scanner.Scan() {
+		if len(scanner.Text()) == 0 {
+			continue
+		} else {
+			output = append(output, scanner.Text())
+		}
 	}
-	if len(scanner.Text()) == 0 {
-		return "", fmt.Errorf("task cannot be blank")
-	}
-	return scanner.Text(), nil
+	return output, nil
 }

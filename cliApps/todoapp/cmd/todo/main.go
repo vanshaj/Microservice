be0edb0b04ed55696975b6f0d81f2dc487f04f3d
vanshaj <vanshajdhar@gmail.com>
2022-10-19 11:45:53 +0530

    Added code to add task from stdin

--- cliApps/todoapp/cmd/todo/main.go
+++ cliApps/todoapp/cmd/todo/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	todo "github.com/vanshaj/Microservice/cliApps/todoapp"
 )
@@ -13,11 +16,24 @@ func main() {
 	list := flag.Bool("list", false, "List all tasks")
 	task := flag.String("task", "", "Task to be included in the todo list")
 	complete := flag.String("complete", "", "Item to be completed")
+	add := flag.Bool("add", false, "Add Task to the Todo List")
 	flag.Parse()
 
 	l := &todo.List{}
 
 	switch {
+	case *add:
+		l.Get(fileName)
+		taskData, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(taskData)
+		if err := l.Save(fileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	case *list:
 		l.Get(fileName)
 		for _, item := range *l {
@@ -46,4 +62,19 @@ func main() {
 		}
 	}
 
+}
+
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	if len(scanner.Text()) == 0 {
+		return "", fmt.Errorf("task cannot be blank")
+	}
+	return scanner.Text(), nil
 }

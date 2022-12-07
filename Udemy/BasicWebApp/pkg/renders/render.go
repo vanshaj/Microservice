72fc3e79525648e5d3f7052a0c434a5203d8a2be
vanshaj_aqua <vanshajdhar@gmail.com>
2022-12-07 17:10:36 +0530

    Created a base template to reuse

--- Udemy/BasicWebApp/pkg/renders/render.go
+++ Udemy/BasicWebApp/pkg/renders/render.go
@@ -8,7 +8,7 @@ import (
 
 func RenderTemplate(w http.ResponseWriter, path string) {
 	fmt.Println("LOGGER - filePath", path)
-	parseTemplate, _ := template.ParseFiles(path)
+	parseTemplate, _ := template.ParseFiles(path, "./templates/base.page.tmpl")
 	err := parseTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Fprintln(w, "Unable to render template")

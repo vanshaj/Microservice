9e653065bc15c67d4dbccc6f208479950315a711
root <vanshajdhar@gmail.com>
2023-01-09 12:30:06 +0000

    Added comments

--- cliApps/csvParser/main.go
+++ cliApps/csvParser/main.go
@@ -56,30 +56,39 @@ func run(filenames []string, operation string, column int, dir bool, w io.Writer
 			filesCh <- filename
 		}
 	}()
+	// Use of intermediate channel jobsCh
+	// The closing of this intermediate channel will cause the worker function to retrieve empty filenames in case
 	go func() {
 		for {
 			select {
 			case fn := <-filesCh:
+				// When filesCh is closed then case will read the default string value from the channel
+				// This code block will get execute when all the filenames have been processed, so we now close the jobsCh
 				if fn == "" {
 					log.Println("closing job channel positive")
 					close(jobsCh)
 					return
 				}
+				// send filename to jobsCh , positive scenario
 				jobsCh <- fn
 			case <-quit:
+				// This quit channed will get called when there is an error in the processing of file names
 				log.Println("closing job channel negative")
 				close(jobsCh)
 				return
 			}
 		}
 	}()
+	// Worker function
 	for i := 0; i < runtime.NumCPU(); i++ {
 		wg.Add(1)
 		go func() {
+			// Once return is called from for block we will call wg.Done()
 			defer wg.Done()
 			for {
 				select {
 				case filename := <-jobsCh:
+					// When job channel is closed filename will be empty string thus we will return all the goroutines
 					if filename == "" {
 						log.Println("Job channel closed returning goroutine")
 						return
@@ -98,11 +107,15 @@ func run(filenames []string, operation string, column int, dir bool, w io.Writer
 			}
 		}()
 	}
+	// Keep on listening, [till len of filenames]
 	for i := 0; i < len(filenames); i++ {
 		select {
 		case err := <-errCh:
+			// if we have an error we don't want to perform anythin so we close the quit channel which will triger the case of line 74 case <- quit and this close the jobCh
 			close(quit)
 			go func() {
+				// We will return all goroutines because jobCh is closed and filename in line 92 will be empty but what about some goroutines who are  in progress
+				// For those we will just listen to the noise and continue in backgound
 				for {
 					select {
 					case <-resCh:
@@ -112,6 +125,7 @@ func run(filenames []string, operation string, column int, dir bool, w io.Writer
 					}
 				}
 			}()
+			// Wait for all worker goroutines to return
 			wg.Wait()
 			close(resCh)
 			close(errCh)
@@ -120,6 +134,7 @@ func run(filenames []string, operation string, column int, dir bool, w io.Writer
 			consolidate = append(consolidate, data...)
 		}
 	}
+	// Wait for all worker goroutines to complete the task and return
 	wg.Wait()
 	_, err := fmt.Fprintln(w, opFunc.operation(consolidate))
 	return err
